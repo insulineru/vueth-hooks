34af6f05e409e8b116d5161ee126c0b89bcccd46
Aleksey Lisun <insuline.ru@gmail.com>
2022-06-18 20:56:56 +0800

    ➕ deps: add ethers, web3-modal. configure vite and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
+const isProd = process.env.NODE_ENV === 'production'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), !isProd
+      && nodePolyfills({
+        include: ['node_modules/**/*.js', /node_modules\/.vite\/.*js/],
+      }),
+  ],
+
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodePolyfills(),
+      ],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
