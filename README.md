9ce438e515f7a45a213a00b149c8ad3ac46f64ec
Aleksey Lisun <insuline.ru@gmail.com>
2022-06-18 16:56:41 +0800

    ⚡️ initial commit ⚡️

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vueth-hooks
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/matryx/vuethereum/master/static/logo-300x300.png" alt="Vuethereum Logo"/>
+</p>
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import EthersProvider from './lib/provider/EthersProvider.vue'
+</script>
+
+<template>
+  <EthersProvider>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello Vue 3 + Vueth-hooks + TypeScript + Vite +" />
+  </EthersProvider>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
